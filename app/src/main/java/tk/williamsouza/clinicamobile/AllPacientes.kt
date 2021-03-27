62fca9c913d8b437b91fd28de4722c836d06a876
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-03-27 19:34:31 -0300

    created new and all pacientes activities

---
+++ app/src/main/java/tk/williamsouza/clinicamobile/AllPacientes.kt
@@ -0,0 +1,11 @@
+package tk.williamsouza.clinicamobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AllPacientes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_pacientes)
+    }
+}
---
+++ app/src/main/java/tk/williamsouza/clinicamobile/NewPaciente.kt
@@ -0,0 +1,11 @@
+package tk.williamsouza.clinicamobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewPaciente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_paciente)
+    }
+}
